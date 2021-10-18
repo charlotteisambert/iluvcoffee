4d9549dfa0ddc3e938b32b1d3be5d7e5d854e1fb
Charlotte Isambert <charlotte.isambert@gmail.com>
2021-10-18 09:11:43 +0200

    (database) add dynamic module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      // 👈 passing in dynamic values
+      type: 'postgres',
+      host: 'localhost',
+      password: 'password',
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
