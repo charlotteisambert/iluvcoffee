8d315ce6828ee8eafc52ed90ebef0b51447db5d3
Charlotte Isambert <charlotte.isambert@gmail.com>
2021-10-19 18:28:55 +0200

    (app spec) fix end to end tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,9 +16,14 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) // 👈
       .get('/')
+      .set('Authorization', process.env.API_KEY) // 👈
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello Nest!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
